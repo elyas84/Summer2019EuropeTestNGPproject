d6d7b15aa357717253ae52e827c6d2d06125c98d
Elyas Arkin <elyas.arkin@hjotmail.se>
2019-11-17 21:49:34 +0100

    day3 TestCase

---
+++ src/test/java/com/cybertek/tests/day7Types_of_Element/DisableElement.java
@@ -0,0 +1,39 @@
+package com.cybertek.tests.day7Types_of_Element;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class DisableElement {
+    @Test
+            public void test1() throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+       WebElement greenradioButton = driver.findElement(By.id("green"));
+        System.out.println("green radioButton is enable = " + greenradioButton.isEnabled());
+        Assert.assertFalse(greenradioButton.isEnabled());
+        greenradioButton.click();
+
+
+    }
+
+    @Test
+    public void test2(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        WebElement inputBox = driver.findElement(By.cssSelector("#input-example>input"));
+        System.out.println("inputBox is enable= " + inputBox.isEnabled());
+
+        driver.quit();
+
+
+    }
+
+
+
+}
---
+++ src/test/java/com/cybertek/tests/day7Types_of_Element/RadioBottunTest.java
@@ -0,0 +1,38 @@
+package com.cybertek.tests.day7Types_of_Element;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class RadioBottunTest {
+    @Test
+    public void radioButton(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        WebElement blueButton = driver.findElement(By.id("blue"));
+        WebElement redButton = driver.findElement(By.id("red"));
+        // How to cheak radio button selected or not?
+
+
+        System.out.println("redButton = " + redButton.isSelected());
+        System.out.println("blueButton = " + blueButton.isSelected());
+
+        Assert.assertTrue(blueButton.isSelected(),"Verify that blue button is selected");
+        Assert.assertFalse(redButton.isSelected(),"Verify that red button is not selected");
+
+       redButton.click();
+        System.out.println("redButton is Selected= " + redButton.isSelected());
+        System.out.println("blueButton is selected = " + blueButton.isSelected());
+
+        Assert.assertFalse(blueButton.isSelected(),"Verify that blue button is not selected");
+        Assert.assertTrue(redButton.isSelected(),"Verify that red button is selected");
+
+        driver.quit();
+
+    }
+
+}
