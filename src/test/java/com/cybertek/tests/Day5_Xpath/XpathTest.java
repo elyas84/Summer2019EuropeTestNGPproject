be099e217afdd886b1989368835155d61e4165b9
Elyas Arkin <elyas.arkin@hotmail.se>
2019-12-09 22:00:34 +0100

    updated

--- src/test/java/com/cybertek/tests/Day5_Xpath/XpathTest.java
+++ src/test/java/com/cybertek/tests/Day5_Xpath/XpathTest.java
@@ -13,8 +13,25 @@ public class XpathTest {
 
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
 
-        WebElement homeLink = driver.findElement(By.xpath("/html/body/nav/ul/li/a")); //Xpath Absolute way, from the root to child
+      /*  WebElement homeLink = driver.findElement(By.xpath("/html/body/nav/ul/li/a")); //Xpath Absolute way, from the root to child
         homeLink.click();
+        */
+      WebElement button1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+      button1.click();
+      WebElement text= driver.findElement(By.xpath("//h3[.='Multiple buttons']"));
+      String printText = text.getText();
+        System.out.println("printText = " + printText);
+
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement signUpButton = driver.findElement(By.xpath("//button[.='Sign Up']"));
+        String getText =signUpButton.getText();
+        System.out.println("getText = " + getText);
+        WebElement signUpPage = driver.findElement(By.xpath("//h2[.='Sign Up Page']"));
+        String newText = signUpPage.getText();
+        System.out.println("newText = " + newText);
+
+
+
 
 
 
