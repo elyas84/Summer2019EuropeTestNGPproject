de9fda963be9b859b3cbdce9c29608395812d384
Elyas Arkin <elyas.arkin@hotmail.se>
2019-12-23 11:45:00 +0100

    updated

--- src/test/java/com/cybertek/tests/Day_16_Review/verifyContactInfoTest.java
+++ src/test/java/com/cybertek/tests/Day_16_Review/verifyContactInfoTest.java
@@ -14,18 +14,23 @@ public class verifyContactInfoTest extends TestBase {
    @Test
 
     public void contactDetailsTest(){
-
+      extentLogger = report.createTest("Verify full name");
        LoginPage loginPage = new LoginPage();
+
        String username = ConfigurationReader.get("driver_username");
+       extentLogger.info("Enter username");
        String password = ConfigurationReader.get("driver_password");
+       extentLogger.info("Enter password");
        loginPage.login(username,password);
 
        DashboardPage dashboardPage = new DashboardPage();
        dashboardPage.navigateToModule("Customers","Contacts");
+       extentLogger.info("Navigate to Customers and Contacts");
        dashboardPage.waitUntilLoaderScreenDisappear();
 
 
         ContactsPage contactsPage = new ContactsPage();
+        extentLogger.info("Click the email link");
         contactsPage.getContactEmail("mbrackstone9@example.com").click();
        contactsPage.waitUntilLoaderScreenDisappear();
 
@@ -33,14 +38,20 @@ public class verifyContactInfoTest extends TestBase {
 
 
        contactsPage.waitUntilLoaderScreenDisappear();
+       extentLogger.info("Getting the actual full name");
        String actualFullName = contactInfoPage.fullName.getText();
+       extentLogger.info("Expected full name");
        String expectedFullName ="Ms Mariam Brackstone";
 
+       extentLogger.pass("actual full name and expected full name is equal");
        Assert.assertEquals(actualFullName,expectedFullName);
 
+       extentLogger.info("Actual email");
        String actualEmail = contactInfoPage.email.getText();
+       extentLogger.info("Expected email");
        String expectedEmail = "mbrackstone9@example.com";
 
+       extentLogger.pass("Actual email and expected email is matching");
        Assert.assertEquals(actualEmail,expectedEmail,"Verify that email is matching");
 
 
